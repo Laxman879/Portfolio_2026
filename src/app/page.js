be08be241c03ee33f93f846ea69196820593bb55
Laxman879 <laxman.a@brewcode.co>
2026-02-03 16:29:10 +0530

    Fix: Remove unused React hooks from server component

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-import { Suspense, useEffect, useState } from "react";
+import { Suspense } from "react";
 import ClientAboutView from "@/components/client-view/about";
 import ClientContactView from "@/components/client-view/contact";
 import ClientExperienceAndEducationView from "@/components/client-view/experience";
