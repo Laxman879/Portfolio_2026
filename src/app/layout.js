9a3ad734d9589d92a560bc18cd50c497bbfefead
Laxman879 <laxman.a@brewcode.co>
2026-02-03 16:42:03 +0530

    Add GTM implementation

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import Analytics from '@/components/analytics'
 import AnalyticsTracker from '@/components/analytics/tracker'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 
 const inter = Inter({ 
   subsets: ['latin'],
@@ -25,8 +26,31 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-head"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NP896R68');`,
+          }}
+        />
       </head>
       <body className={`${inter.className} antialiased`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NP896R68"
+            height="0" 
+            width="0" 
+            style={{display: 'none', visibility: 'hidden'}}
+          />
+        </noscript>
+        
         <CommonLayout>{children}</CommonLayout>
         <Analytics />
         <AnalyticsTracker />
